1ed39b1720a84e1d076cdd5c9821e7dc311451ec
RomarioSantos-Oficial <limaromario40@gmail.com>
2024-05-02 14:36:04 -0300

    00

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     var musica = document.getElementById('musica');
-    musica.volume = 0.1; // Define o volume para 10% (0.0 a 1.0)
+    musica.volume = 0.0; // Define o volume para 10% (0.0 a 1.0)
     musica.play();
 
     var botao = document.getElementById('meuBotao');
     botao.addEventListener('click', function() {
-        window.location.href = './src/html/home.html'; // Substitua 'outra_pagina.html' pelo nome da sua página de destino
+        window.location.href = './src/pages/home.html'; // Substitua 'outra_pagina.html' pelo nome da sua página de destino
     });
 });
